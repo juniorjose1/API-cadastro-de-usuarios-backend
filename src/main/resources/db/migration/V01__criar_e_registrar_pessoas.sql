0b868d9a5a9d2d0492cd8c129c379f673bc53804
juniorjose1 <jajrjunior95@gmail.com>
2019-11-10 00:10:03 -0300

    Atualizado 09-11-2019

--- src/main/resources/db/migration/V01__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_pessoas.sql
@@ -5,3 +5,34 @@
 		sexo VARCHAR(10) NOT NULL,
 		grupo VARCHAR(20) NOT NULL
 		)ENGINE=InnODB DEFAULT CHARSET=UTF8;
+		
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('José Alexandre', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Abraão', 'Adulto', 'Homem', 'Amigos do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Rebeca', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Rosana', 'Criança/Adolescente', 'Mulher', 'Família do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Bruna', 'Bebê', 'Mulher', 'Amigos em Comum');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Kevin', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Paulo Roberto', 'Adulto', 'Homem', 'Amigos do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Arthur Santos', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Heitor', 'Criança/Adolescente', 'Mulher', 'Família do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Vitor Ribeiro', 'Bebê', 'Mulher', 'Amigos em Comum');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Lauro', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Carlos Alberto', 'Adulto', 'Homem', 'Amigos do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Messi', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Cristiano Ronaldo', 'Criança/Adolescente', 'Mulher', 'Família do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Bruna', 'Bebê', 'Mulher', 'Amigos em Comum');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Manuela', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Renata Paiva', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Nathalia Alkmin', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Laura', 'Criança/Adolescente', 'Mulher', 'Família do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Mauro', 'Bebê', 'Mulher', 'Amigos em Comum');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Pedro', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Maria José', 'Adulto', 'Homem', 'Amigos do Noivo');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Tathiana', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Rosana', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Rodrygo', 'Bebê', 'Mulher', 'Amigos em Comum');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Marquinhos', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Diego', 'Adulto', 'Homem', 'Amigos da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('David', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Leandro', 'Criança/Adolescente', 'Mulher', 'Família da Noiva');
+		INSERT INTO pessoa(nome, idade, sexo, grupo) values ('Vitória', 'Bebê', 'Mulher', 'Amigos em Comum');
